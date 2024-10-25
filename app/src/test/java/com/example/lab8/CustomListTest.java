04030c08cd78d4d6649df10b582930e61b4f3ae7
“Newthe” <jhe22@ualberta.ca>
2024-10-25 03:47:53 -0600

    Complete hasCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,6 +4,9 @@ package com.example.lab8;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 //import org.junit.Before;
